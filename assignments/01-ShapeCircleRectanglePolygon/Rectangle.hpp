4e1d9805d827a847d4988498646b57077066b883
A01742102FerPerez <A01742102@tec.mx>
2022-05-24 14:30:15 +0000

    Listo el rectangulo

---
+++ assignments/01-ShapeCircleRectanglePolygon/Rectangle.hpp
@@ -0,0 +1,15 @@
+#pragma Once
+#include "Shape.hpp"
+
+class Rectangle : public Shape
+{
+private:
+    int alto;
+    int ancho;
+public:
+    Rectangle();
+    Rectangle(int,int);
+
+    int getAlto();
+    int getAncho();
+};
